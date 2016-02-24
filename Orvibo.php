148332308c457467b8a405bb1648677826ce3cbf
fso <fso@vsibiri.info>
2016-02-24 18:54:26 +0300

    fixed binary data, arr2bin added, changed constructor parameters order

--- Orvibo.php
+++ Orvibo.php
@@ -32,38 +32,35 @@ class Orvibo
   private $mac;
   private $delay = 10000; //microseconds
   private $subscribed=false;
-  private $twenties = array(0x20,0x20,0x20,0x20,0x20,0x20);
-  private $zeroes = array(0x00,0x00,0x00,0x00);
+  private $twenties = array('20','20','20','20','20','20');
+  private $zeroes = array('00','00','00','00');
   
-  public function __construct($host = '10.10.100.254', $port = 10000, $mac)
+  public function __construct($mac, $host, $port = 10000)
   {	 
    $this->host = $host;
    $this->port = $port;
-   $this->mac = $mac;
+   $this->mac  = preg_split('/:/', $mac, -1, PREG_SPLIT_NO_EMPTY);
    if ($this->subscribed === false) {
       $this->subscribe();
     }
    }
-
   public function getDelay()
   {
     return $this->delay;
   }
-
   private function subscribe() {
-    $command = array(0x68,0x64,0x00,0x1e,0x63,0x6c);
+    $command = array('68', '64', '00', '1e', '63', '6c');
     $command = array_merge($command, $this->mac, $this->twenties, array_reverse($this->mac),
 			   $this->twenties);
     $this->sendCommand($command);
     $this->subscribed=true;
   }
-
   public function on() {
    if ($this->subscribed === false) {
       $this->subscribe();
     }
-    $command = array(0x68,0x64,0x00,0x17,0x64,0x63);
-    $command = array_merge($command, $this->mac, $this->twenties, $this->zeroes, array(0x01));
+    $command = array('68', '64', '00', '17', '64', '63');
+    $command = array_merge($command, $this->mac, $this->twenties, $this->zeroes, array('01'));
     $this->sendCommand($command);    
   }
   
@@ -71,20 +68,29 @@ class Orvibo
    if ($this->subscribed === false) {
       $this->subscribe();
     }
-    $command = array(0x68,0x64,0x00,0x17,0x64,0x63);
-    $command = array_merge($command, $this->mac, $this->twenties, $this->zeroes, array(0x00));
+    $command = array('68', '64', '00', '17', '64', '63');
+    $command = array_merge($command, $this->mac, $this->twenties, $this->zeroes, array('00'));
     $this->sendCommand($command);    
   }
   
   public function sendCommand(Array $command)
   {
-    $message = vsprintf(str_repeat('%c', count($command)), $command);
-    for ($try=0;$try<5;$try++) {
+    $message = $this->arr2bin($command);
+    for ($try=0; $try < 5; $try++) {
       if ($socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP)) {
         socket_sendto($socket, $message, strlen($message), 0, $this->host, $this->port);
         socket_close($socket);
         usleep($this->getDelay()); //wait 100ms before sending next command
       }
     }
+  }
+
+  public function arr2bin(array $arr)
+  {
+    $bytes = "";
+    foreach($arr as $a) {
+        $bytes .= chr(hexdec($a));
+    }
+    return $bytes;
   }
 }
